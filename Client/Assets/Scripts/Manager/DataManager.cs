5462cba7f6ee82ad9fc360a8d52d884cb2a4dbc4
1zero0 <1824668442@qq.com>
2025-03-16 16:09:48 +0800

    EnemyDefine,LevelSummonDefine,WeaponDefine三个表的配置以及读取代码

--- Client/Assets/Scripts/Manager/DataManager.cs
+++ Client/Assets/Scripts/Manager/DataManager.cs
@@ -10,6 +10,13 @@ using Newtonsoft.Json;
 public class DataManager : Singleton<DataManager>
 {
     public string DataPath;
+    // EnemyDefine 的读取
+    public Dictionary<int, EnemyDefine> Enemies;
+    // WeaponDefine 的读取
+    public Dictionary<int, WeaponDefine> Weapons;
+    // LevelSummonDefine 的读取
+    //public Dictionary<int, LevelSummonDefine> LevelSummons;
+
 
 
     public DataManager()
@@ -23,6 +30,17 @@ public class DataManager : Singleton<DataManager>
         //string json = File.ReadAllText(this.DataPath + "Contents.txt");
         //this.Contents = JsonConvert.DeserializeObject<Dictionary<int, ContentDefine>>(json);
 
+        // EnemyDefine 的读取
+        string json1 = File.ReadAllText(this.DataPath + "EnemyDefine.txt");
+        this.Enemies = JsonConvert.DeserializeObject<Dictionary<int, EnemyDefine>>(json1);
+
+        // WeaponDefine 的读取
+        string json2 = File.ReadAllText(this.DataPath + "WeaponDefine.txt");
+        this.Weapons = JsonConvert.DeserializeObject<Dictionary<int, WeaponDefine>>(json2);
+
+        // LevelSummonDefine 的读取
+        //string json3 = File.ReadAllText(this.DataPath + "LevelSummonDefine.txt");
+        //this.LevelSummons = JsonConvert.DeserializeObject<Dictionary<int, LevelSummonDefine>>(json2);
     }
 
     public IEnumerator LoadData()
@@ -31,6 +49,25 @@ public class DataManager : Singleton<DataManager>
         //this.Contents = JsonConvert.DeserializeObject<Dictionary<int, ContentDefine>>(json);
 
         yield return null;
+
+        // EnemyDefine 的读取
+        string json1 = File.ReadAllText(this.DataPath + "EnemyDefine.txt");
+        this.Enemies = JsonConvert.DeserializeObject<Dictionary<int, EnemyDefine>>(json1);
+
+        yield return null;
+
+        // WeaponDefine 的读取
+        string json2 = File.ReadAllText(this.DataPath + "WeaponDefine.txt");
+        this.Weapons = JsonConvert.DeserializeObject<Dictionary<int, WeaponDefine>>(json2);
+
+        yield return null;
+
+        // LevelSummonDefine 的读取
+        //string json3 = File.ReadAllText(this.DataPath + "LevelSummonDefine.txt");
+        //this.LevelSummons = JsonConvert.DeserializeObject<Dictionary<int, LevelSummonDefine>>(json3);
+
+        //yield return null;
+
     }
 
 }
